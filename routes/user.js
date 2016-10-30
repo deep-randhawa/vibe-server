ad755f866d6eb2f66d47bc9d4af7bd4ef3a75b20
Deep Randhawa <deepsaranr@gmail.com>
2016-10-30 16:39:15 -0400

    Added route to add new user data

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,36 @@
-const Users   = require('../collections/users.js')
+const Users   = require('../collections/users')
+const User    = require('../models/user')
 const Logger  = require('../util/log.js')
 const Router  = require('express').Router();
 
-Router.get('/users', function(req, res, next) {
-  Users.forge()
-  .fetch()
-  .then(function(collection) {
-    res.json({error: false, data: collection.toJSON()});
-  })
-  .catch(function(err) {
-    res.status(500).json({
-      error: true,
-      data: {
-        message: err.message
-      }
+Router
+  .get('/', function(req, res, next) {
+    Users.forge()
+      .fetch()
+      .then(function(collection) {
+        res.json({error: false, data: collection.toJSON()});
+      })
+      .catch(function(err) {
+        res.status(500).json({
+          error: true,
+          data: {
+            message: err.message
+          }
+        })
+      });
+
+Router
+  .post('/', function(req, res, next) {
+    User.forge({
+      name: req.body.name,
+      email: req.body.email
+    })
+    .save()
+    .then(function(user) {
+      res.json({error: false, data: {id: user.get('id')}});
+    })
+    .catch(function(err) {
+      res.status(500).json({error: true, data: {message: err.message}});
     })
   })
 })
